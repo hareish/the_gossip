3a187f40b9f2f9ac2c7a68e663da4d3e513ed231
hareish <hareish3@gmail.com>
2021-05-10 18:04:46 +0200

    modif1

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+
+     def team
+  end
+
+  def contact  	
+  end
+
+  def welcome  	
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+ get '/team', to: 'static_pages#team'
+ get '/contact', to: 'static_pages#contact'
+ get '/welcome/:name', to: 'static_pages#welcome', as: 'welcome'
+
+end
